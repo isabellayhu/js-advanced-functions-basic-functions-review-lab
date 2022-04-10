ead1b0c76a2c7aaa0d7d7e6d9917dc8f9d83f584
Isabella <isabella.y.hu@gmail.com>
2022-04-10 10:35:28 +1000

    commit tree 67bcae0b6fb9bda5060ee7b47fa541c504629cf6

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(character="*"){
+    return function(emphatic){
+        return `You are ${character}${emphatic}${character}!`;
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+      return a+b;
+    },
+    subtract: function(a,b){
+        return a-b;
+    },
+    multiply: function(a,b){
+      return a*b;
+    },
+    divide: function(a,b){
+      return a/b;
+    }
+}
+
+function actionApplyer (a, arrayOfTransforms) {
+    let result = a;
+    for (let transform of arrayOfTransforms) {
+        result = transform(result);
+    }
+    return result;
+}
